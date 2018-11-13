e6522be31d8b3fb21ef50d0b797dd329d134799e
Jeremy <josuexrl19@gmail.com>
2018-11-12 19:06:16 -0600

    PrimerComit

---
+++ src/main/java/com/mycompany/labintegracion/Main.java
@@ -0,0 +1,14 @@
+package com.mycompany.labintegracion;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        
+        
+    }//fin main
+    
+}
